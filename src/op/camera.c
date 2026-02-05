45e555d34e1fb3ed052aa53c2af335893f4d3e4b
Miko <tarmiko2025@gmail.com>
2026-02-05 22:04:30 +0700

    Improved Camera Control

--- src/op/camera.c
+++ src/op/camera.c
@@ -8,6 +8,7 @@ static void action_move(struct map_entity_action_data *data){
 
 }
 
+
 static struct map_entity_action actions[] = {
 	{
 		.name = "move",
@@ -23,13 +24,50 @@ static void on_fps30(struct entity_op_data *data){
 	//printf("CAMERA_FPS30\n");
 }
 
+static Uint32 held[256] = { 0 };
 static void on_tick(struct entity_op_data *data){
-	//printf("CAMERA_TICK\n");
-	struct map_entity *entity = data->entity;
+	//struct map_entity *camera = data->entity;
+	struct map_entity *camera = map_center;
 	/*printf("CAMERA: %.2f %.2f\n",
 		entity->position->x,
 		entity->position->y
 	);*/
+
+// Inside your main loop
+	/*const bool* state = SDL_GetKeyboardState(NULL);
+	if(state[SDL_SCANCODE_A]) {
+		camera->position->x -= 20;
+	}
+	else if(state[SDL_SCANCODE_S]) {
+		camera->position->y += 20;
+	}
+	else if(state[SDL_SCANCODE_W]) {
+		camera->position->y -= 20;
+	}
+	else if(state[SDL_SCANCODE_D]) {
+		camera->position->x += 20;
+	}*/
+
+
+	for(size_t i = 0; held[i] != 0; i++){
+		//printf("(%i -> %i) %i]", held[i], SDLK_A, held[i] == SDLK_A);
+		if(held[i] == (Uint32) SDLK_A){
+			camera->position->x -= 20;
+		}
+		else if(held[i] == (Uint32) SDLK_S){
+			camera->position->y += 20;
+		}
+		else if(held[i] == (Uint32) SDLK_W){
+			camera->position->y -= 20;
+		}
+		else if(held[i] == (Uint32)SDLK_D){
+			camera->position->x += 20;
+		} else {
+			continue ;
+		}
+		//printf("%i ", held[i]);
+		fflush(stdout);
+	}
 }
 
 static void on_init(struct entity_op_data *data){
@@ -56,11 +94,38 @@ static void on_new(struct entity_op_data *data){
 static void on_event(struct entity_op_data *data){
 	SDL_Event *event = data->event;
 	struct map_entity *camera = map_center;
+	size_t empty = 0;
+	if((event->type != SDL_EVENT_KEY_UP)
+		&& (event->type != SDL_EVENT_KEY_DOWN)
+	){
+		return ;
+	}
+
+	while(held[empty] != 0){
+		if(held[empty] == event->key.key){
+			if(event->type == SDL_EVENT_KEY_UP){
+				held[empty] = 1;
+			} else {
+				return ;
+			}
+		}
+
+		empty++;
+	}
+
+	empty = 0;
+
+	while(held[empty] > 2){
+		empty++;
+	}
+
 	if(event->type == SDL_EVENT_KEY_DOWN){
+		held[empty] = event->key.key;
+
 		//printf("DOWN [%.4s\n", (char *)(&event->key));
 		//const char *keyName = SDL_GetKeyName(event->key.key);
 		//printf("DOWN [%s]\n", keyName);
-		if(event->key.key == SDLK_A){
+		/*if(event->key.key == SDLK_A){
 			camera->position->x -= 20;
 		}
 		else if(event->key.key == SDLK_S){
@@ -71,7 +136,7 @@ static void on_event(struct entity_op_data *data){
 		}
 		else if(event->key.key == SDLK_D){
 			camera->position->x += 20;
-		}
+		}*/
 	}
 }
 
